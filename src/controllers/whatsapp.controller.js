ffed1231cd5b1a678c780974c25d16021b343b71
Jhosef Reyes <jhosefgrey@gmail.com>
2023-01-03 16:54:13 -0600

    Send reply

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,11 +1,78 @@
+const fs = require("fs");
+const myConsoloe = new console.Console(fs.createWriteStream('./logs.txt'))
+const whp = require("../services/whatsapp.service")
+
+
+require('dotenv').config()
+
 const verifyToken = (req, res) =>{
-    res.send("Hola verify Token")
+
+    try {
+        let token = req.query["hub.verify_token"];
+        let challenge = req.query["hub.challenge"];
+
+        if(challenge != null && token != null && token === process.env.TOKEN){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
+
+    } catch (e) {
+        res.status(400).send();
+    }
 }
 
 const recivedMessage = (req, res) =>{
-    res.send("Hola Received")
+   try {
+
+    let entry = (req.body["entry"])[0];
+    let changes = (entry["changes"])[0];
+    let value = changes["value"];
+    let messageObject = value["messages"];
+   
+    if(typeof messageObject != "undefined"){
+        let text = GetTextUser(messageObject[0])
+        let number = (messageObject[0])["from"] 
+
+        myConsoloe.log(messageObject);
+        whp.SendMessageWh("Udted Dijo: " + text, number);
+
+    }
+
+    res.send("EVENT_RECEIVED")
+   } catch (e) {
+    myConsoloe.log(e);
+    res.send("EVENT_RECEIVED")
+   }
 }
 
+
+function GetTextUser(message){
+    let tex = "";
+    let typeMeesage = message["type"];
+
+    switch (typeMeesage) {
+        case "text":
+            tex = (message["text"])["body"]
+            break;
+            case "interactive":
+            
+            let interactiveObject = message["interactive"];
+            let typeInteractive = interactiveObject["type"];
+
+            myConsoloe.log(interactiveObject);
+
+            if(typeInteractive === "button_reply"){
+                tex = (interactiveObject["button_reply"])["title"];
+            }else{
+                tex = (interactiveObject["list_reply"])["title"];
+            }
+
+            break;
+    }
+
+    return tex;
+}
 
 module.exports ={
     verifyToken,
---
+++ src/services/whatsapp.service.js
@@ -0,0 +1,43 @@
+const https = require("https");
+require('dotenv').config()
+
+function SendMessageWh(textResponse, number) {
+    const data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": number,
+        "type": "text",
+        "text": {
+            "preview_url": false,
+            "body": textResponse
+        }
+    });
+
+    const options = {
+        host: "graph.facebook.com",
+        path: "/v15.0/113148464973818/messages",
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "bearer " + process.env.TW
+        }
+    }
+
+    const req = https.request(options, res => { 
+        res.on("data", d => { 
+            process.stdout.write(d) }); 
+        });
+
+    req.on("error", error =>{
+        console.error(error)
+    })
+
+    req.write(data);
+    req.end();
+}
+
+
+module.exports ={
+    SendMessageWh
+}
