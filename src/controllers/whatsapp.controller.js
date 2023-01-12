73dcc304aff64184395d6713856de61acaadcabb
Jhosef Reyes <jhosefgrey@gmail.com>
2023-01-12 14:58:37 -0600

    ver por que no responde los botones

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -43,6 +43,7 @@ const recivedMessage = (req, res) => {
                 req.body.entry[0].changes[0].value.messages[0]
             ) {
 
+                
                 let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
                 let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
                 let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; 
